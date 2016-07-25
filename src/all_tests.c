cea4d947c33b28a4f38e6bb7121e67b3b61a1af7
Nikolaus Howe <nhowe@rna.williams.edu>
2016-07-25 16:38:35 -0400

    added all_tests.c

---
+++ src/all_tests.c
@@ -0,0 +1,29 @@
+#include "EnergyModel.h"
+#include "RNA.h"
+#include "DataFile.h"
+#include "EnergyFunctions.h"
+#include "PartitionFunction.h"
+#include "AllowedPairs.h"
+#include "MacrofoldConsole.h"
+#include "StochasticSamples.h"
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+
+
+void auPenaltyTest() {
+  // load up files
+
+  // check if energies are the same0
+  printf("AU Penalty Test Failed <Details>\n");
+  exit(-1);
+}
+
+
+int main(int argc, char* argv[]) {
+
+  auPenaltyTest();
+  hairpinTermTest();
+  
+
+}
